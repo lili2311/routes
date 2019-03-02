2131316cdcf7d1ce1e2c6b4e567411fe7b355818
jspc <james@zero-internet.org.uk>
2019-03-02 11:46:37 +0000

    Initialise repo

---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+package routes is a small, no-frills routing library for fasthttp. It's designed to sit within a fasthttp
+aware service to determine which route, from a map, to direct a ctx at.
+
+It is designed:
+1. To contain no third party module (beyond fasthttp)
+2. To be as unobtrusive as possible
+
+*/
+package routes
